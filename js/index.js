9a4e3c575763644513f1d4309e067fb13aebe7ac
ElianaCorbalan <eli_corbalan@hotmail.com.ar>
2021-05-24 18:36:28 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+    sr.reveal('.navbar', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.main__header__title' ,{
+        duration: 3000,
+        origin: 'left',
+        distance: '-400px'
+    });
+
+    sr.reveal('.parrafo' ,{
+        duration: 2000,
+        origin: 'right',
+        distance: '-400px'
+    });
+
+    sr.reveal('.img__inicio' ,{
+        duration: 5000,
+        rotate: {
+            x: 1,
+            y: 180
+        }
+    });
