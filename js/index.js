92ed70f051ca7444902f557a4c5968292b65abf0
ElianaCorbalan <eli_corbalan@hotmail.com.ar>
2021-06-14 16:50:36 -0300

    Actualizacion de proyecto con mejor visualizacion de contenido

--- js/index.js
+++ js/index.js
@@ -1,26 +1,13 @@
 window.sr = ScrollReveal();
-    sr.reveal('.navbar', {
-        duration: 3000,
+    sr.reveal('.menu', {
+        duration: 2000,
         origin: 'bottom',
         distance: '-100px'
     });
 
-    sr.reveal('.main__header__title' ,{
-        duration: 3000,
-        origin: 'left',
-        distance: '-400px'
-    });
 
-    sr.reveal('.parrafo' ,{
+    sr.reveal('.subtitulo' ,{
         duration: 2000,
         origin: 'right',
         distance: '-400px'
-    });
-
-    sr.reveal('.img__inicio' ,{
-        duration: 5000,
-        rotate: {
-            x: 1,
-            y: 180
-        }
     });
